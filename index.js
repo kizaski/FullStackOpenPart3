d671a7e16c4bbf2ac95a9046eca56c5186b476cb
Kiril <kiril2315@gmail.com>
2023-07-12 21:25:17 +0300

    notes

--- index.js
+++ index.js
@@ -30,6 +30,20 @@ app.get( '/api/notes', ( request, response ) =>
     response.json( notes )
 } )
 
+app.get( '/api/notes/:id', ( request, response ) =>
+{
+    const id = Number( request.params.id )
+    const note = notes.find( note => note.id === id )
+
+    if ( note )
+    {
+        response.json( note )
+    } else
+    {
+        response.status( 404 ).end()
+    }
+} )
+
 const PORT = 3001
 app.listen( PORT, () =>
 {
