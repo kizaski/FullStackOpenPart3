3ce118214002fbe72080c483e7d217a01b49a8b9
Kiril <kiril2315@gmail.com>
2023-07-12 21:07:31 +0300

    express

--- index.js
+++ index.js
@@ -1,28 +1,37 @@
-const http = require('http')
+const express = require( 'express' )
 
+const app = express()
 
 let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    important: true
-  },
-  {
-    id: 2,
-    content: "Browser can execute only JavaScript",
-    important: false
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
-  }
+    {
+        id: 1,
+        content: "HTML is easy",
+        important: true
+    },
+    {
+        id: 2,
+        content: "Browser can execute only JavaScript",
+        important: false
+    },
+    {
+        id: 3,
+        content: "GET and POST are the most important methods of HTTP protocol",
+        important: true
+    }
 ]
-const app = http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'application/json' })
-  response.end(JSON.stringify(notes))
-})
+
+app.get( '/', ( request, response ) =>
+{
+    response.send( '<h1>Hello World!</h1>' )
+} )
+
+app.get( '/api/notes', ( request, response ) =>
+{
+    response.json( notes )
+} )
 
 const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+app.listen( PORT, () =>
+{
+    console.log( `Server running on port ${ PORT }` )
+} )
