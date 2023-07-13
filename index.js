8863914f7b0fad99a0a9cc4cb197dcd9cd5e5ab5
Kiril <kiril2315@gmail.com>
2023-07-13 13:56:19 +0300

    info

--- index.js
+++ index.js
@@ -32,6 +32,17 @@ app.get( '/', ( request, response ) =>
     response.send( '<h1>Hello World!</h1>' )
 } )
 
+app.get( '/info', ( request, response ) =>
+{
+    now = new Date()
+    response.send( 
+    `
+        <p>Phonebook has info for ${persons.length} people </p>
+        <p>${now}</p>
+    ` )
+} )
+
+
 app.get( '/api/persons', ( request, response ) =>
 {
     response.json( persons )
