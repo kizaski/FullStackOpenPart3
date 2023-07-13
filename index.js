5307a75578177ac96febd6a6f8dd50c94d828faf
Kiril <kiril2315@gmail.com>
2023-07-13 19:14:27 +0300

    kors

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require( 'express' )
 const morgan = require( 'morgan' )
+const cors = require('cors')
+
 
 const app = express()
 
 app.use( express.json() )
+
+app.use(cors())
 
 morgan.token( 'body', req =>
 {
@@ -120,7 +124,7 @@ app.post( '/api/persons', ( request, response ) =>
     response.json( person )
 } )
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen( PORT, () =>
 {
     console.log( `Server running on port ${ PORT }` )
