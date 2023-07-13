d9f2af359859917866f6f80ddea894b23fb53350
Kiril <kiril2315@gmail.com>
2023-07-13 13:50:32 +0300

    persons

--- index.js
+++ index.js
@@ -4,21 +4,26 @@ const app = express()
 
 app.use(express.json())
 
-let notes = [
-    {
-        id: 1,
-        content: "HTML is easy",
-        important: true
+let persons = [
+    { 
+      id: 1,
+      name: "Arto Hellas", 
+      number: "040-123456"
     },
-    {
-        id: 2,
-        content: "Browser can execute only JavaScript",
-        important: false
+    { 
+      id: 2,
+      name: "Ada Lovelace", 
+      number: "39-44-5323523"
     },
-    {
-        id: 3,
-        content: "GET and POST are the most important methods of HTTP protocol",
-        important: true
+    { 
+      id: 3,
+      name: "Dan Abramov", 
+      number: "12-43-234345"
+    },
+    { 
+      id: 4,
+      name: "Mary Poppendieck", 
+      number: "39-23-6423122"
     }
 ]
 
@@ -27,37 +32,40 @@ app.get( '/', ( request, response ) =>
     response.send( '<h1>Hello World!</h1>' )
 } )
 
-app.get( '/api/notes', ( request, response ) =>
+app.get( '/api/persons', ( request, response ) =>
 {
-    response.json( notes )
+    response.json( persons )
 } )
 
-app.get( '/api/notes/:id', ( request, response ) =>
+app.get( '/api/persons/:id', ( request, response ) =>
 {
     const id = Number( request.params.id )
-    const note = notes.find( note => note.id === id )
+    const person = persons.find( p => p.id === id )
 
-    if ( note )
+    if ( person )
     {
-        response.json( note )
+        response.json( person )
     } else
     {
         response.status( 404 ).end()
     }
 } )
 
-app.delete( '/api/notes/:id', ( request, response ) =>
+app.delete( '/api/persons/:id', ( request, response ) =>
 {
     const id = Number( request.params.id )
-    notes = notes.filter( note => note.id !== id )
+    persons = persons.filter( p => p.id !== id )
 
     response.status( 204 ).end()
 } )
 
-app.post('/api/notes', (request, response) => {
-  const note = request.body
-  console.log(note)
-  response.json(note)
+app.post('/api/persons', (request, response) => {
+  const person = request.body
+  console.log(person)
+  response.json(person)
+  //
+  //
+  // ...
 })
 
 const PORT = 3001
