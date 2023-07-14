af8158c9829778a72a817fa689811991947e1d25
Kiril <kiril2315@gmail.com>
2023-07-14 17:13:30 +0300

    mongoose

---
+++ mongo.js
@@ -0,0 +1,35 @@
+const mongoose = require( 'mongoose' )
+
+if ( process.argv.length < 3 )
+{
+    console.log( 'give password as argument' )
+    process.exit( 1 )
+}
+
+const password = process.argv[ 2 ]
+const name = process.argv[ 3 ]
+const number = process.argv[ 4 ]
+
+const url =
+    `mongodb+srv://kizaski:${ password }@cluster0.npuskb8.mongodb.net/noteApp?retryWrites=true&w=majority`
+
+mongoose.set( 'strictQuery', false )
+mongoose.connect( url )
+
+const personSchema = new mongoose.Schema( {
+    name: String,
+    number: String,
+} )
+
+const Person = mongoose.model( 'Person', personSchema )
+
+const person = new Person( {
+    name: name,
+    number: number,
+} )
+
+person.save().then( result =>
+{
+    console.log( `added ${result.name} ${result.number} to phonebook\n` )
+    mongoose.connection.close()
+} )
